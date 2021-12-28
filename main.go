f0c3e836f6b12ffe2ab3e7bdd91e6121c24360d6
dmitrytaranov <taranovdmitry0813@gmail.com>
2021-12-28 03:58:17 +0200

    add program

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	var input int
 	fmt.Scan(&input)
 	fmt.Print("Set time to shutdown in seconds: ")
+
 	_, err := os.CreateTemp("shutdown.bat", fmt.Sprintf("shutdown /s /t %d", input))
 	if err != nil {
 		fmt.Errorf("failed to create file: %s", err)
