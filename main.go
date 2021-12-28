a3df4eb0c097b3df0f5fd6df2809300d28a8294b
DmytroTaranov <taranovdmitry0813@gmail.com>
2021-12-29 01:37:18 +0200

    add scanner, add file remover, add file path, broke into functions

--- main.go
+++ main.go
@@ -1,17 +1,56 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"os/exec"
+	"strconv"
 )
 
 func main() {
-	var input int
-	fmt.Scan(&input)
-	fmt.Print("Set time to shutdown in seconds: ")
-
-	_, err := os.CreateTemp("shutdown.bat", fmt.Sprintf("shutdown /s /t %d", input))
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Set time to shutdown in minutes: ")
+	scanner.Scan()
+	text := scanner.Text()
+	textInt, err := strconv.Atoi(text)
 	if err != nil {
-		fmt.Errorf("failed to create file: %s", err)
+		fmt.Printf("failed to convert the string: %s", err)
 	}
+	textInt *= 60
+
+	command := fmt.Sprintf("shutdown /s /t %d", textInt)
+
+	file, err := createFile()
+	path := filePath(err)
+	file.WriteString(command)
+	execFile(path, err)
+
+	defer os.Remove("shutdowner.bat")
+	defer file.Close()
+
+}
+
+func execFile(path string, err error) {
+	exec.Command(`cmd.exe`, `/C`, path+"\\shutdowner.bat").Run()
+	if err != nil {
+		fmt.Printf("failed to remove file %s", err)
+	}
+}
+
+func filePath(err error) string {
+	path, err := os.Getwd()
+	if err != nil {
+		fmt.Printf("failedt to get path: %s", err)
+	}
+	return path
+}
+
+func createFile() (*os.File, error) {
+	file, err := os.OpenFile("shutdowner.bat", os.O_RDWR|os.O_CREATE, 0755)
+	if err != nil {
+		fmt.Printf("failed to create file %s", err)
+		os.Exit(1)
+	}
+	return file, err
 }
